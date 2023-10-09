831eb99cf1e5a5d6e92e7f5bb623fa5b8382e30c
restuadkh <restuadimas11@yahoo.com>
2023-10-09 10:44:02 +0700

    update

--- controllers/DbController.js
+++ controllers/DbController.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: "localhost", // Ganti dengan host MySQL Anda
-  user: "root", // Ganti dengan nama pengguna MySQL Anda
-  password: "", // Ganti dengan kata sandi MySQL Anda
+  user: "restu", // Ganti dengan nama pengguna MySQL Anda
+  password: "hokya4", // Ganti dengan kata sandi MySQL Anda
   database: "wa", // Ganti dengan nama database Anda
 });
 
